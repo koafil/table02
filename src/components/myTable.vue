a1c3f83875a288adcffb274d2f2930e752bb9a0f
kfil <koafil@gmail.com>
2024-02-08 11:01:49 +0300

    вывод бренда и страны

--- src/components/myTable.vue
+++ src/components/myTable.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="card">
 <!--    Keys: {{ findKeyArr }}-->
+<!--    {{ data1.rowData[1] }}-->
+<!--    {{ brands }}-->
     <DataTable :value="products"
                @page="onPage($event)"
                lazy paginator
@@ -8,10 +10,20 @@
                :rowsPerPageOptions="[5, 10, 20, 50]"
                tableStyle="min-width: 50rem"
                :totalRecords="totalRecords"
-               :loading="loading"
+               :loading="loading && loadingBrand"
     >
       <Column field="id" header="ID"></Column>
       <Column field="articul" header="Артикул"></Column>
+      <Column field="brand" header="Бренд">
+        <template #body="slotProps">
+          {{ brands.get(slotProps.data.brand)?.Name }}
+        </template>
+      </Column>
+      <Column field="brand" header="Страна">
+        <template #body="slotProps">
+          {{ brands.get(slotProps.data.brand)?.Country }}
+        </template>
+      </Column>
       <Column field="info" header="Инфо"></Column>
       <template #footer> Всего {{ totalRecords ? totalRecords : 0 }} позиций. </template>
     </DataTable>
@@ -27,6 +39,8 @@ import { ref, onMounted, computed} from 'vue';
 //onMounted(() => {
 //  ProductService.getProductsMini().then((data) => (products.value = data));
 //});
+//onMounted(() => {
+//});
 // const props = defineProps({
 //   findKeyArr: Object,
 // })
@@ -34,6 +48,9 @@ const props = defineProps(["findKeyArr"])
 const totalRecords = ref(0)
 const rows = ref(10);
 const page = ref(0);
+const brands_mass =ref ([]);
+
+//const loading = ref(true);
 
 const products = ref([
   {
@@ -50,6 +67,11 @@ const products = ref([
 // onMounted ( () => {
 //   loading.value = true;
 // });
+const brands = computed(()=>{
+  let m = new Map();
+  brands_mass.value.forEach(o=>{m.set(o.id, { Name: o?.Name, Country: o?.Country})})
+  return m;
+})
 const url = computed(()=>{
   // return `http://192.168.50.5:3002/tovars?page=${page.value}&npp=${limit.value}&s=${findKey.value}`
 //  return `http://192.168.50.5:3002/tovars?page=${page.value}&npp=${rows.value}`
@@ -62,7 +84,7 @@ const url = computed(()=>{
 //  return `http://192.168.50.5:3002/tovars?page=${page.value}&npp=${rows.value}&ss=${encodeURI(JSON.stringify(props.findKeyArr))}`
 //  return `http://192.168.50.5:3002/tovars?page=${page.value}&npp=${rows.value}&ss=${props.findKeyArr}`
 })
-const {isFetching:loading, error, data } = useFetch(url, {
+const {isFetching:loading, error, data:data0 } = useFetch(url, {
   refetch: true,
   afterFetch(ctx){
     totalRecords.value=ctx.data.total;
@@ -70,6 +92,13 @@ const {isFetching:loading, error, data } = useFetch(url, {
     return ctx
   }
 }).json();
+const {isFetching:loadingBrand, error:error1, data:data1 } = useFetch(`http://192.168.50.5:3002/brands`, {
+  afterFetch(ctx){
+    brands_mass.value = ctx.data.rowData;
+    return ctx
+  }
+}).json();
+
 
 const onPage = (event) => {
   rows.value=event.rows;
